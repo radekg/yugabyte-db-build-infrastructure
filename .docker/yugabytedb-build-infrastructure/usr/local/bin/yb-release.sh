4a22c238c02e7bf2822b06f4c88606ecdfc449d9
radekg <radek@gruchalski.com>
2021-08-04 18:15:09 +0200

    Fix the release build move command, build version file name does not always match the request branch / tag

--- .docker/yugabytedb-build-infrastructure/usr/local/bin/yb-release.sh
+++ .docker/yugabytedb-build-infrastructure/usr/local/bin/yb-release.sh
@@ -4,5 +4,5 @@ cd /yb-source
 yes | ./yb_release
 # remove old build, if exists...
 rm -rf /yb-source/build/yugabyte-${YB_VERSION}.tar.gz
-mv /yb-source/build/yugabyte-${YB_VERSION}*.tar.gz /yb-source/build/yugabyte-${YB_VERSION}.tar.gz
+mv /yb-source/build/yugabyte-*.tar.gz /yb-source/build/yugabyte-${YB_VERSION}.tar.gz
 echo "Your build is available in /yb-source/build/yugabyte-${YB_VERSION}.tar.gz"

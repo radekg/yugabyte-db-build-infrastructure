19dcefd258fbe9e9c1a7427a1b44667f8c51cfad
radekg <radek@gruchalski.com>
2021-08-04 23:15:19 +0200

    Fix the order of commands in distribution build

--- .docker/yugabytedb-build-infrastructure/usr/local/bin/yb-release.sh
+++ .docker/yugabytedb-build-infrastructure/usr/local/bin/yb-release.sh
@@ -2,10 +2,10 @@
 set -eu
 # working directory
 cd /yb-source
-# create the release archive
-yes | ./yb_release
 # remove old build, if exists...
 rm -rf /yb-source/build/yugabyte-*.tar.gz
+# create the release archive
+yes | ./yb_release
 # move to a well known location
 mv /yb-source/build/yugabyte-*.tar.gz /yb-source/build/yugabyte-${YB_RELEASE_VERSION}.tar.gz
 # done
